2dbfda280f681386c996e667c7ff508719775299
peacefrog <tawsif93@gmail.com>
2017-09-30 21:21:23 +0600

    File Extensions

---
+++ src/main/java/com/spl3/lips/files/FileExtension.java
@@ -0,0 +1,23 @@
+package com.spl3.lips.files;
+
+/**
+ * Created by peacefrog on 9/30/17.
+ * Time 9:13 PM
+ */
+public enum FileExtension {
+
+	java("java"),
+	C("c"),
+	python("py");
+
+
+	private String value;
+
+	FileExtension(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
