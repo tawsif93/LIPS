36b5464823156eb153010b7525b7617569ff4df6
peacefrog <tawsif93@gmail.com>
2017-11-28 20:22:36 +0600

    path updated

--- example/orig/glue.py
+++ example/orig/glue.py
@@ -6,9 +6,9 @@ currency = "?"
 decimal = ","
 
 if use_locale:
-  currency = commands.getoutput('./reader 0')
-  decimal = commands.getoutput('./reader 1')
+  currency = commands.getoutput('./work/reader.cout 0')
+  decimal = commands.getoutput('./work/reader.cout 1')
 
-cmd = ('java checker ' + currency
+cmd = ('java -cp work checker ' + currency
        + sys.argv[1] + decimal + sys.argv[2])
 print commands.getoutput(cmd)
